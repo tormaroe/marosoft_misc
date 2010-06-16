d48e08f2c683d6c83238526df70a182f4bd43614
Torbjorn Maro <torbjorn.maro@gmail.com>
2010-06-16 16:03:53 +0200

    Cleaned up sender (exception handling not needed)

--- pswincom/send_incoming_sms/Program.cs
+++ pswincom/send_incoming_sms/Program.cs
@@ -4,40 +4,27 @@ namespace send_incoming_sms
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            using (var sender = new SMSReceive { Url = args[0] })
+            {
+                var result = sender.ReceiveSMSMessage(new IncomingSMSMessage
+                {
+                    SenderNumber = Prompt("Sender number"),
+                    ReceiverNumber = Prompt("Receiver number"),
+                    Text = Prompt("Message text"),
+                });
+
+                Console.WriteLine("Message sent!");
+                Console.WriteLine("Return code {0}. Description: {1}. Reference: {2}",
+                    result.Code, result.Description, result.Reference);
+            }
+        }
+
         private static string Prompt(string label)
         {
             Console.Write(label + ": ");
             return Console.ReadLine();
-        }
-
-        static void Main(string[] args)
-        {
-            if (args.Length != 1)
-            {
-                Console.WriteLine("Usage: send_incoming_sms.exe URL_to_SMS_endpoint");
-                return;
-            }
-
-            try
-            {
-                using (var sender = new SMSReceive { Url = args[0] })
-                {
-                    var result = sender.ReceiveSMSMessage(new IncomingSMSMessage
-                    {
-                        SenderNumber = Prompt("Sender number"),
-                        ReceiverNumber = Prompt("Receiver number"),
-                        Text = Prompt("Message text"),
-                    });
-
-                    Console.WriteLine("Message sent!");
-                    Console.WriteLine("Return code {0}. Description: {1}. Reference: {2}",
-                        result.Code, result.Description, result.Reference);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("EXCEPTION: {0}", ex.Message);                
-            }
         }
     }
 }
